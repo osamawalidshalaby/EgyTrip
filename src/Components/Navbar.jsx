82bbe565c35240aebd052221014cf518afe00a3f
rahma khaled <126900186+rahma-khaled@users.noreply.github.com>
2025-12-04 20:30:08 +0200

    merge branch 'main' into master

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
-=======
-// Navbar Component
-
->>>>>>> main
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import logo from '../assets/logo.png';
-<<<<<<< HEAD
 import './Navbar.css'; 
 
 function Navbar({ menuOpen, setMenuOpen }) {
@@ -76,36 +70,6 @@ function Navbar({ menuOpen, setMenuOpen }) {
             </button>
             </a>
           </div>
-=======
-
-
-function Navbar({ menuOpen, setMenuOpen }) {
-  return (
-    <nav className="navbar navbar-expand-lg bg-white shadow-sm py-3">
-      <div className="container-fluid">
-        <img
-          src={logo}
-          alt="Logo"
-          width="40"
-          height="40"
-          className="d-inline-block align-text-top"
-        />
-        <a className="navbar-brand fw-bold text-primary fs-3">TourBooker</a>
-
-        <button className="navbar-toggler" type="button" onClick={() => setMenuOpen(!menuOpen)}>
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div className={`collapse navbar-collapse ${menuOpen ? "show" : ""}`}>
-          <ul className="navbar-nav ms-auto mb-2 mb-lg-0 gap-3">
-            <li className="nav-item"><a className="nav-link">Home</a></li>
-            <li className="nav-item"><a className="nav-link">Destinations</a></li>
-            <li className="nav-item"><a className="nav-link">About</a></li>
-            <li className="nav-item"><a className="nav-link">Contact</a></li>
-          </ul>
-          <button className="btn btn-outline-primary ms-3">Login</button>
-          <button className="btn btn-primary ms-2">Sign Up</button>
->>>>>>> main
         </div>
       </div>
     </nav>
